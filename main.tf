38fc22d0a616b1fdf1573f0d413dd63adcff560f
Neil Daly <neildaly2142@gmail.com>
2025-01-07 11:42:24 +0000

    refactor: :recycle: use official security group module

--- main.tf
+++ main.tf
@@ -27,42 +27,20 @@ resource "aws_instance" "blog" {
     Name = "HelloWorld"
   }
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 }
 
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "Allow ingress http and https traffic. Allow all egress."
+# https://registry.terraform.io/modules/terraform-aws-modules/security-group/aws/latest?tab=outputs
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.2.0"
+  name    = "blog"
 
   vpc_id = data.aws_vpc.default.id
-}
 
-resource "aws_security_group_rule" "blog_http_ingress" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = [http-80-tcp, https-443-tcp]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_ingress" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_egress" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
+  egress_rules       = [all-all]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
