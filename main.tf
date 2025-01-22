65a3a3314c2e1d0497ef2dedfa73d198c700f451
Neil Daly <neildaly2142@gmail.com>
2025-01-22 12:18:58 +0000

    fix: azs

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "blog_vpc" {
   name = var.environment.name
   cidr = "${var.environment.network_prefix}.0.0/16"
 
-  azs            = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  azs            = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   public_subnets = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
 
